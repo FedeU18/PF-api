1b553090f0aa48c8bc37256f538f99b7619d394a
Gianella Leon <giigl@hotmail.com>
2022-11-19 21:01:34 -0500

    +

--- src/db.js
+++ src/db.js
@@ -54,7 +54,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const {} = sequelize.models;
+const {Materias} = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
--- src/models/Materias.js
+++ src/models/Materias.js
@@ -1,13 +1,15 @@
-const { DataTypes, UUIDV4 } = require('sequelize');
+
+
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
+  // defino el modelo
   sequelize.define('materias', {
-    
     name: {
-      type:DataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-
-
   },{timestamps: false});
 };
