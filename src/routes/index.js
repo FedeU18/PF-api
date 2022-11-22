c9f8324ea29cc2b8db9bef54f6309071952c8aee
fabian menjura <fabian.menjura.22@gmail.com>
2022-11-22 16:24:28 -0500

    backend finalizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const Materias= require('./materias')
-const Search=require('./search')
-const Country=require("./country")
+const Materias = require("./materias");
+const Search = require("./search");
+const Country = require("./country");
+const Alumnos = require("./alumno.js");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/',Search)
-router.use('/materias',Materias)
-router.use('/paises',Country)
-
+router.use("/", Search);
+router.use("/materias", Materias);
+router.use("/paises", Country);
+router.use("/alumnos", Alumnos);
 module.exports = router;
