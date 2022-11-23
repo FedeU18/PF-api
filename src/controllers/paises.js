d64c642833cab29f2418be939ef5aef6c8c0a33b
fabian lopez <fabianlopez928@gmail.com>
2022-11-23 14:42:21 -0500

    paises_latam

--- src/controllers/paises.js
+++ src/controllers/paises.js
@@ -1,16 +1,26 @@
 const axios = require("axios");
 const { Country } = require("../db.js");
+let paisesLatam = ["Argentina","Bolivia"," Brasil","Chile","Colombia","Costa Rica","Cuba","Ecuador","El Salvador","Guayana Francesa","Granada","Guatemala","Guayana","Haití","Honduras","México","Nicaragua","Paraguay","Panamá","Perú","Puerto Rico","República Dominicana","Uruguay","Venezuela",]
 
 const getApiCountries = async (req, res) => {
   try {
     const apiInfo = await axios.get("https://restcountries.com/v3/all");
-    const apiCountries = await apiInfo.data.map((c) => {
+    const apiCountries = await apiInfo.data.map((ele) => {
       return {
-        name: c.name.common,
-        flag: c.flags[0],
+        name: ele.name.common,
+        flag: ele.flags[0],
       };
     });
-    let paisesApi = await apiCountries;
+    let paisesApi = await apiCountries.filter(ele=>{
+      if(paisesLatam.includes(ele.name)){
+        //console.log("deberia funcionar",ele.name)
+        return{
+          name:ele.name,
+          flag:ele.flag,
+        }
+        
+      }
+    });
 
     paisesApi.forEach((el) => {
       Country.findOrCreate({
@@ -26,3 +36,4 @@ const getApiCountries = async (req, res) => {
 };
 
 module.exports = { getApiCountries };
+
