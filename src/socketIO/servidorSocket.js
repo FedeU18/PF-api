05d3a2579a68d0453acb5eb5a3b775c08fbf51fd
fabian menjura <fabian.menjura.22@gmail.com>
2022-12-08 19:21:04 -0500

    se modifico el backend para que funcionara el chat en el home de el profesor

--- src/socketIO/servidorSocket.js
+++ src/socketIO/servidorSocket.js
@@ -1,38 +1,41 @@
-const { guardarchat } = require("../controllers/chat.js");
-const { mensajesAntiguos } = require("../controllers/chat.js");
-const {usuariosChat}=require("../controllers/chat.js")
-
+const {
+    guardarchat
+} = require("../controllers/chat.js");
+const {
+    mensajesAntiguos
+} = require("../controllers/chat.js");
+const {
+    usuariosChat
+} = require("../controllers/chat.js")
 
 const eventosSocket = (io) => {
-  io.on("connection", (socket) => {
-    console.log("nueva conexion");
+    io.on("connection", (socket) => {
+        console.log("nueva conexion");
+        socket.on("join_room", (data) => {
+            socket.join(data);
+        });
+        socket.on("mensajes_de_profe", data=>{
+          socket.emit("mensajes_de_profe", data)
+        })
 
-    socket.on("join_room", (data) => {
-      socket.join(data);
+        socket.on("mensaje_privado", (data) => {
+          console.log(data)
+            guardarchat(data);
+            io.to(data.room).emit("mensaje_privado", data);
+        });
+        socket.on("mensajes_antiguos", async (userLogin, receptor) => {
+            const mensajes = await mensajesAntiguos(userLogin, receptor);
+            socket.emit("mensajes_antiguos", mensajes)
+            console.log(mensajes);
+        });
+        socket.on("usuarios_chat", async (userLogin) => {
+            let userchat = await usuariosChat(userLogin)
+            console.log("soy usuarios devueltos ",userchat)
+            socket.emit("usuarios_chat", userchat)
+        })
+        socket.on("disconnect", () => {
+            console.log("Usuario desconnectado");
+        });
     });
-
-    socket.on("mensaje_privado", (data) => {
-      guardarchat(data);
-      io.to(data.room).emit("mensaje_privado", data);
-    });
-
-    socket.on("mensajes_antiguos", async (userLogin, receptor) => {
-      const mensajes = await mensajesAntiguos(userLogin, receptor); 
-      socket.emit("mensajes_antiguos", mensajes)    
-    console.log(mensajes);
-    });
-
-
-    socket.on("usuarios_chat", async()=>{
-     let userchat= await usuariosChat()
-     console.log(userchat)
-     socket.emit("usuarios_chat",userchat)
-    })
-
-    socket.on("disconnect", () => {
-      console.log("Usuario desconnectado");
-    });
-  });
 };
-
 module.exports = eventosSocket;
