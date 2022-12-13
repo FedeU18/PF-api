c83266ab246f4db3e51d7de25ceb49fe0aafff94
gastonf-git <flora.gaston@outlook.com>
2022-12-13 01:01:09 -0300

    backend filters with pagination

--- src/routes/profesores.js
+++ src/routes/profesores.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const {getProfesor,getById,postProfe,deleteProfesor,putProfesor} = require("../controllers/profesores");
+const {getProfesor,getById,postProfe,deleteProfesor,putProfesor,fetchProfesor} = require("../controllers/profesores");
 
 const router = Router();
 
 
 router.get("/",getProfesor);
+router.get("/api",fetchProfesor);
 router.get("/:id",getById);
 router.post("/",postProfe);
 router.delete("/:id",deleteProfesor);
